b18bb9d088ab9447c0821d1afc420c9bc0388855
pashavictorovich <pavel@codefresh.io>
2020-10-29 18:27:35 +0200

    export wait sync message

--- pkg/builder/builder.go
+++ pkg/builder/builder.go
@@ -71,7 +71,8 @@ func (b *builder) Sync(args *SyncArgs, name string, authToken string, host strin
 		b.lines = append(b.lines, command)
 	}
 	if args.WaitHealthy {
-		b.lines = append(b.lines, buildCommandWithAllThings(fmt.Sprintf("argocd app wait %s", name), args, authToken, *hostDomain))
+		cmd := buildCommandWithAllThings(fmt.Sprintf("argocd app wait %s", name), args, authToken, *hostDomain)
+		b.lines = append(b.lines, fmt.Sprintf("{ cf_export waitSyncErrorMessage=$(%s 2>&1 >&3 3>&-); } 3>&1"), cmd)
 	}
 	if args.WaitForSuspend {
 		b.lines = append(b.lines, buildCommandWithAllThings(fmt.Sprintf("argocd app wait %s --suspended", name), args, authToken, *hostDomain))
