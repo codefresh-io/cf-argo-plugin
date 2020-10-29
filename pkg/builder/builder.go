bd0e9a6620832102870bada68b06fa822b1704e0
pashavictorovich <pavel@codefresh.io>
2020-10-29 19:07:44 +0200

    temp timeout flag

--- pkg/builder/builder.go
+++ pkg/builder/builder.go
@@ -72,7 +72,8 @@ func (b *builder) Sync(args *SyncArgs, name string, authToken string, host strin
 	}
 	if args.WaitHealthy {
 		cmd := buildCommandWithAllThings(fmt.Sprintf("argocd app wait %s --timeout 60", name), args, authToken, *hostDomain)
-		b.lines = append(b.lines, fmt.Sprintf("{ cf_export waitSyncErrorMessage=$(%s 2>&1 >&3 3>&-); } 3>&1", cmd))
+		b.lines = append(b.lines, fmt.Sprintf("{ waitSyncErrorMessage=$(%s 2>&1 >&3 3>&-); } 3>&1", cmd))
+
 	}
 	if args.WaitForSuspend {
 		b.lines = append(b.lines, buildCommandWithAllThings(fmt.Sprintf("argocd app wait %s --suspended", name), args, authToken, *hostDomain))
