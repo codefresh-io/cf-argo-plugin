330a349828fe7ed6e1d8630952c2a22c010468d1
pashavictorovich <pavel@codefresh.io>
2020-12-14 22:37:36 +0200

    print output to result

--- pkg/bash/command_executor.go
+++ pkg/bash/command_executor.go
@@ -11,7 +11,7 @@ type CommandExecutor struct {
 }
 
 func execCommand(command string) {
-	cmd := exec.Command("/bin/sh", command)
+	cmd := exec.Command("cf_export", command)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
@@ -22,6 +22,6 @@ func execCommand(command string) {
 }
 
 func (commandExecutor CommandExecutor) ExportGitopsInfo(activity codefresh.UpdatedActivity) {
-	execCommand(fmt.Sprintf("cf_export sendArgoMetadata_CF_ENVIRONMENT_ID=\"%s\"", activity.EnvironmentId))
-	execCommand(fmt.Sprintf("cf_export sendArgoMetadata_CF_ACTIVITY_ID=\"%s\"", activity.ActivityId))
+	execCommand(fmt.Sprintf("sendArgoMetadata_CF_ENVIRONMENT_ID=\"%s\"", activity.EnvironmentId))
+	execCommand(fmt.Sprintf("sendArgoMetadata_CF_ACTIVITY_ID=\"%s\"", activity.ActivityId))
 }
