b84ac8c637a440c8a03da8c46d2e3d690a1da50d
pashavictorovich <pavel@codefresh.io>
2020-12-15 18:11:06 +0200

    export env variable to meta

--- pkg/bash/command_executor.go
+++ pkg/bash/command_executor.go
@@ -1,7 +1,6 @@
 package bash
 
 import (
-	"bytes"
 	"cf-argo-plugin/pkg/codefresh"
 	"fmt"
 	"os/exec"
@@ -11,37 +10,14 @@ type CommandExecutor struct {
 }
 
 func execCommand(command string) {
-	cmd := exec.Command("/bin/bash", "cf_export", command)
-	var out bytes.Buffer
-	cmd.Stdout = &out
+	cmd := exec.Command("bash", "-c", "echo "+command+" >> /meta/env_vars_to_export")
 	err := cmd.Run()
-	if err != nil {
-		fmt.Printf("Failed to execute export command: %v\n", err)
-	}
-	fmt.Printf("Command result: %q\n", out.String())
-}
-
-func execCommand2(command string) {
-	cmd := exec.Command("/bin/bash", "echo 1")
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		fmt.Printf("Failed to execute export command: %v\n", err)
-	}
-	fmt.Printf("Command result: %q\n", out.String())
-}
-
-func execCommand3(command string) {
-	cmd := exec.Command("bash", "-c", "echo MY_PLUGIN_VAR=SAMPLE_VALUE >> /meta/env_vars_to_export")
-
-	err := cmd.Run()
-
 	if err != nil {
 		fmt.Printf("Failed to execute export command: %v\n", err)
 	}
 }
 
 func (commandExecutor CommandExecutor) ExportGitopsInfo(activity codefresh.UpdatedActivity) {
-	execCommand3("")
+	execCommand(fmt.Sprintf("sendArgoMetadata_CF_ENVIRONMENT_ID=\"%s\"", activity.EnvironmentId))
+	execCommand(fmt.Sprintf("sendArgoMetadata_CF_ACTIVITY_ID=%s", activity.ActivityId))
 }
