739a74628f472aa8ad4579b855d9db52f8ace4f0
pashavictorovich <pavel@codefresh.io>
2020-12-15 11:43:33 +0200

    change to echo

--- pkg/bash/command_executor.go
+++ pkg/bash/command_executor.go
@@ -1,7 +1,6 @@
 package bash
 
 import (
-	"bytes"
 	"cf-argo-plugin/pkg/codefresh"
 	"fmt"
 	"os/exec"
@@ -12,13 +11,10 @@ type CommandExecutor struct {
 
 func execCommand(command string) {
 	cmd := exec.Command("/bin/bash", "echo", command, ">>", "/meta/env_vars_to_export")
-	var out bytes.Buffer
-	cmd.Stdout = &out
 	err := cmd.Run()
 	if err != nil {
 		fmt.Printf("Failed to execute export command: %v\n", err)
 	}
-	fmt.Printf("Command result: %q\n", out.String())
 }
 
 func (commandExecutor CommandExecutor) ExportGitopsInfo(activity codefresh.UpdatedActivity) {
