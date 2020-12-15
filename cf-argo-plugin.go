d7bea39064c635566ca5abeb0a8828bc5a07a388
pashavictorovich <pavel@codefresh.io>
2020-12-15 13:17:32 +0200

    change to echo

--- cf-argo-plugin.go
+++ cf-argo-plugin.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"os/exec"
-	"strings"
-)
+import "cf-argo-plugin/cmd/root"
 
 func main() {
-	//root.Execute()
-
-	cmd := exec.Command("/bin/bash", "-c", "echo sendArgoMetadata_CF_ENVIRONMENT_ID=3 >> /tmp/env_vars_to_export")
-
-	err := cmd.Run()
-
-	print(cmd.Path)
-	print(strings.Join(cmd.Args, ","))
-
-	if err != nil {
-		println(err.Error())
-	}
-
+	root.Execute()
 }
