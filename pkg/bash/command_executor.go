6e1c8d4cf8d78d2dd9b5d2695d4ba11cb1850cc8
pashavictorovich <pavel@codefresh.io>
2020-12-15 13:26:44 +0200

    change to echo

--- pkg/bash/command_executor.go
+++ pkg/bash/command_executor.go
@@ -11,7 +11,7 @@ type CommandExecutor struct {
 }
 
 func execCommand(command string) {
-	cmd := exec.Command("cf_export", command)
+	cmd := exec.Command("/codefresh/volume/cf_export", command)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
