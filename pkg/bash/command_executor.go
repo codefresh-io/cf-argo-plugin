2998fed343b7689d3e429104bdcbdf16baaace53
pashavictorovich <pavel@codefresh.io>
2020-12-15 16:12:26 +0200

    not change out

--- pkg/bash/command_executor.go
+++ pkg/bash/command_executor.go
@@ -34,13 +34,10 @@ func execCommand2(command string) {
 
 func execCommand3(command string) {
 	cmd := exec.Command("/bin/echo", "MY_PLUGIN_VAR=SAMPLE_VALUE >> /meta/env_vars_to_export")
-	var out bytes.Buffer
-	cmd.Stdout = &out
 	err := cmd.Run()
 	if err != nil {
 		fmt.Printf("Failed to execute export command: %v\n", err)
 	}
-	fmt.Printf("Command result: %q\n", out.String())
 }
 
 func (commandExecutor CommandExecutor) ExportGitopsInfo(activity codefresh.UpdatedActivity) {
