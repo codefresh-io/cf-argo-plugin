b568f8f3b1312f0ad6d42ea8abd9a15c3ef7335f
pashavictorovich <pavel@codefresh.io>
2020-12-15 13:47:30 +0200

    change to echo

--- pkg/bash/command_executor.go
+++ pkg/bash/command_executor.go
@@ -21,7 +21,18 @@ func execCommand(command string) {
 	fmt.Printf("Command result: %q\n", out.String())
 }
 
+func execCommand2(command string) {
+	cmd := exec.Command("/bin/bash", "cf_export", command)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("Failed to execute export command: %v\n", err)
+	}
+	fmt.Printf("Command result: %q\n", out.String())
+}
+
 func (commandExecutor CommandExecutor) ExportGitopsInfo(activity codefresh.UpdatedActivity) {
-	execCommand(fmt.Sprintf("sendArgoMetadata_CF_ENVIRONMENT_ID=%s", activity.EnvironmentId))
+	execCommand(fmt.Sprintf("sendArgoMetadata_CF_ENVIRONMENT_ID=\"%s\"", activity.EnvironmentId))
 	execCommand(fmt.Sprintf("sendArgoMetadata_CF_ACTIVITY_ID=%s", activity.ActivityId))
 }
