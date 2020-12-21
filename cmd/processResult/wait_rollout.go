a8d7ce1a85025b02662c3e42aeb70197e088d1ae
pashavictorovich <pavel@codefresh.io>
2020-12-21 12:03:41 +0200

    increase timeout

--- cmd/processResult/wait_rollout.go
+++ cmd/processResult/wait_rollout.go
@@ -68,7 +68,7 @@ var WaitRolloutCmd = &cobra.Command{
 			time.Sleep(10 * time.Second)
 
 			elapsed := time.Now().Sub(start)
-			if elapsed.Minutes() >= 5 {
+			if elapsed.Minutes() >= 15 {
 				return nil
 			}
 		}
