bd08bbd1fdc47985179e656c2e306d6c1704b9fc
pashavictorovich <pavel@codefresh.io>
2020-12-22 11:52:15 +0200

    add logs

--- cmd/processResult/wait_rollout.go
+++ cmd/processResult/wait_rollout.go
@@ -44,7 +44,7 @@ var WaitRolloutCmd = &cobra.Command{
 			//fmt.Println(fmt.Sprintf("Current in loop history id %v", currentHistoryId))
 			// we identify new rollout
 			if currentHistoryId > historyId {
-				//fmt.Println(fmt.Sprintf("Found new history id %v", currentHistoryId))
+				fmt.Println(fmt.Sprintf("Found new history id %v", currentHistoryId))
 
 				// ignore till we will handle it in correct way, 500 code mean that history not found and we shouldnt break pipeline
 				_, updatedActivities := cf.SendMetadata(&codefresh.ArgoApplicationMetadata{
@@ -69,6 +69,7 @@ var WaitRolloutCmd = &cobra.Command{
 
 			elapsed := time.Now().Sub(start)
 			if elapsed.Minutes() >= 15 {
+				fmt.Println("Stop wait rollout because timeout")
 				return nil
 			}
 		}
